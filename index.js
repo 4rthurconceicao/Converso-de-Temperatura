ac5c64229bff86f61f7e3fe2d23854a1bf9f486a
Arthur Conceição <arthurconceicaosouza1@gmail.com>
2026-04-29 15:04:08 -0300

    add

--- index.js
+++ index.js
@@ -1,46 +1,99 @@
+
+
+ document.addEventListener("keydown", function(evento){
+        if(evento.key === "enter"){
+            convert();
+        }
+    })
+
+
 function convert() {
     let temp = document.getElementById('temp-ratura').value;
     let temp2 = document.getElementById('temp').value;
     let temp3 = document.getElementById('tempe2').value
-    
-   
-  
-     if(temp.trim() === "") {
-        alert('Valor invalido')
-     } else{
-       temp = Number(temp)
-     } 
 
-      
-     
-     if(temp2 === 'C' && temp3 === 'F') {
-        document.getElementById('res').value =(temp.toFixed(2)) * 9/5 + 32 ;
+
+   
+
+
+    if (temp.trim() === "") {
+        alert('Valor invalido')
+    } else {
+        temp = Number(temp)
+    }
+
+
+
+    if (temp2 === 'C' && temp3 === 'F') {
+        let resultado = (temp * 9 / 5) + 32
+
+        if (resultado % 1 === 0) {
+            document.getElementById('res').value = resultado;
+        } else {
+            document.getElementById('res').value = resultado.toFixed(1);
+        }
 
     } else if (temp2 === 'C' && temp3 === 'K') {
-        
-        document.getElementById('res').value = temp.toFixed(2) + 273.15;
+        let resultado = temp + 273.15
+
+        if (resultado % 1 === 0) {
+            document.getElementById('res').value = resultado;
+        } else {
+            document.getElementById('res').value = resultado.toFixed(2);
+        }
+
     } else if (temp2 === 'C' && temp3 === 'C') {
         document.getElementById('res').value = temp;
     }
 
     if (temp2 === 'F' && temp3 === 'C') {
-        document.getElementById('res').value = (temp.toFixed(2) - 32) * 5/9;
+        let resultado = (temp - 32) * 5 / 9
+
+        if (resultado % 1 === 0) {
+            document.getElementById('res').value = resultado;
+        } else {
+            document.getElementById('res').value = resultado.toFixed(1);
+        }
+
+
+
     } else if (temp2 === 'F' && temp3 === 'K') {
-     document.getElementById('res').value = (temp.toFixed(2) - 32) * 5/9 + 273.15;
+
+        let resultado = (temp - 32) * 5 / 9 + 273.15
+
+        if (resultado % 1 === 0) {
+            document.getElementById('res').value = resultado;
+        } else {
+            document.getElementById('res').value = resultado.toFixed(1);
+        }
+
     } else if (temp2 === 'F' && temp3 === 'F') {
         document.getElementById('res').value = temp
     }
 
-     if (temp2 === 'K' && temp3 === 'C') {
-        document.getElementById('res').value = temp.toFixed(2) = 213,15
+    if (temp2 === 'K' && temp3 === 'C') {
+
+        let resultado = temp - 273.15
+
+        if (resultado % 1 === 0) {
+            document.getElementById('res').value = resultado;
+        } else {
+            document.getElementById('res').value = resultado.toFixed(1);
+        }
     } else if (temp2 === 'K' && temp3 === 'F') {
-        document.getElementById('res').value = (temp.toFixed(2) - 273.15) * 9/5 + 32
+
+        let resultado = (temp - 273.15) * 9 / 5 + 32
+
+        if (resultado % 1 === 0) {
+            document.getElementById('res').value = resultado;
+        } else {
+            document.getElementById('res').value = resultado.toFixed(1);
+        }
     } else if (temp2 === 'K' && temp3 === 'K') {
         document.getElementById('res').value = temp
     }
 
 
-    
 
 
 
@@ -51,7 +104,8 @@ function convert() {
 
 
 
-   
+
+
 
 
 
